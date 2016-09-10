8e38874c7d8dcbd1d437e8558f45a534371ee078
gpaulon <giorgiopaulon90@gmail.com>
2016-09-10 15:27:10 -0500

    Exercise 3 initialization

---
+++ HW3/HW3.R
@@ -0,0 +1,56 @@
+
+setwd('~/Desktop/Semester 1/Stat Models for Big Data/SDS385/')
+rm(list=ls())
+
+
+### LINE SEARCH
+
+# (B)
+
+source('../SDS385/HW3/line_search.R')
+
+# TRIAL ON REAL DATA
+
+wdbc <- read.csv(file = '../SDS385-master/data/wdbc.csv', header = F)
+
+y <- as.numeric(wdbc[,2])-1 # 0 for benign, 1 for malign
+N <- length(y)
+X <- cbind(rep(1,N),scale(as.matrix(wdbc[,3:12])))
+colnames(X)[1] <- 'Intercept'
+P <- dim(X)[2]
+mi <- rep(1, N)
+
+glm1 = glm(y~X[,-1], family='binomial')
+
+
+
+maxiter = 100000
+tol = 10E-10
+alpha = 0.01
+beta0 <- rep(0, P)
+
+gradient <- GD_line_search(y, X, mi, beta0, maxiter, tol)
+
+
+
+
+
+
+
+
+
+
+
+
+
+newton <- newton_descent(y, X, mi, beta0, maxiter, tol)
+
+par(mar=c(4,4,2,2))
+plot(gradient$ll, type = 'l', lwd = 2, col = 'red', xlab = 'Iterations', ylab = 'Negative Log-likelihood')
+plot(newton$ll, type = 'l', lwd = 2, col = 'red', xlab = 'Iterations', ylab = 'Negative Log-likelihood')
+
+results <- rbind(glm1$coefficients, gradient$beta, newton$beta)
+for (i in 0:(dim(results)[2]-1))
+  colnames(results)[i+1] <- paste('Beta', i, sep='')
+rownames(results) <- c('Glm','Gradient','Newton')
+
---
+++ HW3/line_search.R
@@ -0,0 +1,114 @@
+
+# DESCENT METHODS
+
+# Compute the probabilities associated with the logit model
+comp_wi <- function(X, beta){
+  wi <- 1/(1+exp(-X%*%beta))
+  return(wi)
+}
+
+# Compute the negative log-likelihood of the logit model
+log_lik <- function(beta, y, X, mi){
+  ll <- array(NA, dim = N)
+  wi <- comp_wi(X, beta)
+  ll <- -sum(y*log(wi+1E-4) + (mi - y)*log(1-wi+1E-4))
+  return(ll)
+}
+
+# Compute the gradient of the negative log-likelihood of the logit model
+grad_loglik <- function(beta, y, X, mi){
+  grad_ll <- array(NA, dim = length(beta))
+  wi <- comp_wi(X, beta)
+  grad_ll <- -apply(X*as.numeric(y - mi*wi), 2, sum)
+  return(grad_ll)
+}
+
+# Compute the hessian of the negative log-likelihood of the logit model
+hessian_loglik <- function(beta, y, X, mi){
+  wi <- comp_wi(X, beta)
+  W_12 <- sqrt(mi*wi*(1-wi))
+  hes_ll <- crossprod(as.numeric(W_12)*X)
+  return(hes_ll)
+}
+
+# Compute the line search
+optimal_step <- function(param_act, direct, ll_act, grad_ll_act, y, X, mi, c=0.01, max_alpha=1, rho=0.5){
+  opt_alpha <- max_alpha
+  while(log_lik(param_act + opt_alpha*direct, y, X, mi) > ll_act + c*opt_alpha*crossprod(grad_ll_act,direct)){
+    opt_alpha <- opt_alpha*rho
+  }
+  return(opt_alpha)
+}
+
+# Function for the gradient descent of the logit model
+GD_line_search <- function(y, X, mi, beta0, maxiter, tol){
+  
+  betas <- array(NA, dim=c(maxiter, length(beta0)))
+  betas[1,] <- beta0 # Initial guess
+  
+  ll <- array(NA, dim = maxiter)
+  ll[1] <- log_lik(betas[1,], y, X, mi)
+  
+  alpha <- array(NA, dim = maxiter)
+  
+  for (iter in 2:maxiter){
+    
+    gradient <- grad_loglik(betas[iter-1,], y, X, mi)
+    direct <- -gradient
+    alpha[iter-1] <- optimal_step(betas[iter-1,], direct, ll[iter-1], gradient, y, X, mi)
+    betas[iter,] <- betas[iter-1,] + alpha[iter-1]*direct
+    ll[iter] <- log_lik(betas[iter,], y, X, mi)
+    
+    # Convergence check
+    if (abs(ll[iter-1] - ll[iter])/abs(ll[iter-1] + 1E-3) < tol){
+      cat('Algorithm has converged after', iter, 'iterations')
+      ll <- ll[1:iter]
+      betas <- betas[1:iter,]
+      break;
+    }
+    
+    else if (iter == maxiter & abs(ll[iter-1] - ll[iter])/abs(ll[iter-1] + 1E-2) >= tol){
+      print('WARNING: algorithm has not converged')
+      break;
+    }
+  }
+  
+  return(list("ll" = ll, "beta" = betas[iter,], "alpha"=alpha[1:(iter-1)]))
+}
+
+# Function for the Newton method of the logit model
+quasi_newton <- function(y, X, mi, beta0, maxiter, tol){
+  
+  betas <- array(NA, dim=c(maxiter, length(beta0)))
+  betas[1,] <- beta0 # Initial guess
+  
+  ll <- array(NA, dim = maxiter)
+  ll[1] <- log_lik(betas[1,], y, X, mi)
+  
+  for (iter in 2:maxiter){
+    
+    hessian <- hessian_loglik(betas[iter-1,], y, X, mi)
+    gradient <- grad_loglik(betas[iter-1,], y, X, mi)
+    
+    # Solve the linear system H^(-1)%*%Grad in order to find the direction
+    direct <- QR_solver(hessian, gradient)
+    
+    betas[iter,] <- betas[iter-1,] - direct
+    ll[iter] <- log_lik(betas[iter,], y, X, mi)
+    
+    # Convergence check
+    if (sqrt(sum((betas[iter,] - betas[iter-1,])^2)) < tol){
+      cat('Algorithm has converged after', iter, 'iterations')
+      ll <- ll[1:iter]
+      betas <- betas[1:iter,]
+      break;
+    }
+    
+    else if (iter == maxiter & sqrt(sum((betas[iter,] - betas[iter-1,])^2)) >= tol){
+      print('WARNING: algorithm has not converged')
+      break;
+    }
+  }
+  
+  return(list("ll" = ll, "beta" = betas[iter,]))
+}
