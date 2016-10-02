eaba9c63013f232d11c4963bb53131f0e3a15ac1
gpaulon <giorgiopaulon90@gmail.com>
2016-10-02 12:21:09 -0500

    Reorganisation files

--- HW2/HW2.R
+++ HW2/HW2.R
@@ -4,8 +4,9 @@ rm(list=ls())
 
 ### STOCHASTIC GRADIENT DESCENT FOR LOGISTIC REGRESSION
 
+# We load the functions
 source('../SDS385/HW1/lin_solver.R')
-source('../SDS385/HW1/descent_methods.R')
+source('../SDS385/HW1/gradient_newton.R')
 source('../SDS385/HW2/sgd.R')
 
 # (C)
@@ -21,6 +22,7 @@ colnames(X)[1] <- 'Intercept'
 P <- dim(X)[2]
 mi <- rep(1, N)
 
+# We compute the GLM result in order to compare different algorithms
 glm1 = glm(y~X[,-1], family='binomial')
 glm1$coefficients
 
@@ -29,13 +31,13 @@ tol = 1E-10
 alpha = 0.01
 beta0 <- rep(0, P)
 
-# Newton method using, as a diagnostic of convergence, the log-likelihood of the 
+# Gradient descent using, as a diagnostic of convergence, the log-likelihood of the 
 # entire dataset
-gradient <- gradient_descent(y, X, mi, beta0, maxiter, alpha, tol)
+gradient <- gradient.descent(y, X, mi, beta0, maxiter, alpha, tol)
 
 # Newton method using, as a diagnostic of convergence, the log-likelihood of the 
 # entire dataset
-newton <- newton_descent(y, X, mi, beta0, maxiter, tol)
+newton <- newton.descent(y, X, mi, beta0, maxiter, tol)
 
 # SGD method using a constant step size alpha and, as a diagnostic of convergence, 
 # the simple moving average of the individual log-likelihoods
@@ -51,33 +53,30 @@ plot(sgd$ll, type = 'l', lwd = 2, col = 'red', xlab = 'Iterations', ylab = 'Nega
 
 # (D)
 
-# SGD method using a Robbins-Monro rule for step sizes and, as a diagnostic of 
-# convergence, the simple moving average of the individual log-likelihoods
-# alpha <- 0.99
-# C <- 8000
-# t0 <- 2000
-
 alpha <- 0.8
 C <- 4000
 t0 <- 1000
 
+# We display the step size as a function of iterations
 plot(1:maxiter,C*(1:maxiter+t0)^(-alpha),type='l',xlab='Iterations',ylab='',main=bquote('Values of'~gamma^(t)),lwd=2)
 
-sgd_robbins <- SGD_Robbins(y, X, mi, beta0, maxiter, C, t0, alpha, tol)
+# SGD method using a Robbins-Monro rule for step sizes and, as a diagnostic of 
+# convergence, the simple moving average of the individual log-likelihoods
+sgd.robbins <- SGD.Robbins(y, X, mi, beta0, maxiter, C, t0, alpha, tol)
 
 par(mar=c(4,2,2,2),mfrow=c(3,4))
 for (i in 1:11){
-  plot(sgd_robbins$beta[seq(1,maxiter,by=40),i],type='l',ylab='',xlab='Iterations',main=bquote('Values of'~beta[.(i)]))
+  plot(sgd.robbins$beta[seq(1,maxiter,by=40),i],type='l',ylab='',xlab='Iterations',main=bquote('Values of'~beta[.(i)]))
   abline(h=newton$beta[i],col='red',lwd=2)
 }
 
-plot(sgd_robbins$ll, type = 'l', lwd = 2, col = 'red', xlab = 'Iterations', ylab = 'Negative individual Log-likelihood')
+plot(sgd.robbins$ll, type = 'l', lwd = 2, col = 'red', xlab = 'Iterations', ylab = 'Negative individual Log-likelihood')
 
 # (E)
 
 par(mar=c(4,2,2,2),mfrow=c(3,4))
 for (i in 1:11){
-  plot(sgd_robbins$beta[seq(maxiter/2,maxiter,by=20),i],type='l',ylab='',xlab='Iterations',main=bquote('Values of'~beta[.(i)]))
+  plot(sgd.robbins$beta[seq(maxiter/2,maxiter,by=20),i],type='l',ylab='',xlab='Iterations',main=bquote('Values of'~beta[.(i)]))
   abline(h=newton$beta[i],col='red',lwd=2)
 }
 
@@ -96,5 +95,5 @@ rownames(results) <- c('Glm','Gradient','Newton','SGD','SGD Robbins-Monro','SGD 
 plot(gradient$ll, type = 'l', lwd = 2, col = 'red', xlab = 'Iterations', ylab = 'Negative Log-likelihood')
 plot(newton$ll, type = 'l', lwd = 2, col = 'red', xlab = 'Iterations', ylab = 'Negative Log-likelihood')
 plot(sgd$ll, type = 'l', lwd = 2, col = 'red', xlab = 'Iterations', ylab = 'Negative individual Log-likelihood')
-plot(sgd_robbins$ll, type = 'l', lwd = 2, col = 'red', xlab = 'Iterations', ylab = 'Negative Log-likelihood')
+plot(sgd.robbins$ll, type = 'l', lwd = 2, col = 'red', xlab = 'Iterations', ylab = 'Negative Log-likelihood')
 
--- HW2/sgd.R
+++ HW2/sgd.R
@@ -17,37 +17,37 @@ SGD <- function(y, X, mi, beta0, maxiter, alpha, tol){
   betas <- array(NA, dim=c(maxiter, length(beta0)))
   betas[1,] <- beta0 # Initial guess
   
-  av_ll <- array(NA, dim = maxiter)
-  av_ll[1] <- log_lik(betas[1,], matrix(y[idx[1]],nrow=1), matrix(X[idx[1],],nrow=1), mi[idx[1]])
+  av.ll <- array(NA, dim = maxiter)
+  av.ll[1] <- log.lik(betas[1,], matrix(y[idx[1]],nrow=1), matrix(X[idx[1],],nrow=1), mi[idx[1]])
   
   for (iter in 2:maxiter){
     
-    gradient <- grad_loglik(betas[iter-1,], matrix(y[idx[iter]],nrow=1), matrix(X[idx[iter],],nrow=1), mi[idx[iter]])
+    gradient <- grad.loglik(betas[iter-1,], matrix(y[idx[iter]],nrow=1), matrix(X[idx[iter],],nrow=1), mi[idx[iter]])
     betas[iter,] <- betas[iter-1,] - alpha*gradient
-    ll <- log_lik(betas[iter,], matrix(y[idx[iter]],nrow=1), matrix(X[idx[iter],],nrow=1), mi[idx[iter]])
-    av_ll[iter] <- ((av_ll[iter-1])*(iter-1) + ll)/iter
+    ll <- log.lik(betas[iter,], matrix(y[idx[iter]],nrow=1), matrix(X[idx[iter],],nrow=1), mi[idx[iter]])
+    av.ll[iter] <- ((av.ll[iter-1])*(iter-1) + ll)/iter
 
     # Convergence check
-    if (abs(av_ll[iter-1] - av_ll[iter])/(av_ll[iter-1] + 1E-10) < tol){
+    if (abs(av.ll[iter-1] - av.ll[iter])/(av.ll[iter-1] + 1E-10) < tol){
       cat('Algorithm has converged after', iter, 'iterations')
-      av_ll <- av_ll[1:iter]
+      av.ll <- av.ll[1:iter]
       betas <- betas[1:iter,]
       break;
     }
 
-    else if (iter == maxiter & abs(av_ll[iter-1] - av_ll[iter])/(av_ll[iter-1] + 1E-10) >= tol){
+    else if (iter == maxiter & abs(av.ll[iter-1] - av.ll[iter])/(av.ll[iter-1] + 1E-10) >= tol){
       print('WARNING: algorithm has not converged')
       break;
     }
     
   }
   
-  return(list("ll" = av_ll, "beta" = betas))
+  return(list("ll" = av.ll, "beta" = betas))
 }
 
 
 # Function for the gradient descent of the logit model
-SGD_Robbins <- function(y, X, mi, beta0, maxiter, C, t0, alpha, tol){
+SGD.Robbins <- function(y, X, mi, beta0, maxiter, C, t0, alpha, tol){
   
   N <- dim(X)[1]
   
@@ -57,31 +57,31 @@ SGD_Robbins <- function(y, X, mi, beta0, maxiter, C, t0, alpha, tol){
   betas <- array(NA, dim=c(maxiter, length(beta0)))
   betas[1,] <- beta0 # Initial guess
   
-  av_ll <- array(NA, dim = maxiter)
-  av_ll[1] <- log_lik(betas[1,], matrix(y[idx[1]],nrow=1), matrix(X[idx[1],],nrow=1), mi[idx[1]])
+  av.ll <- array(NA, dim = maxiter)
+  av.ll[1] <- log.lik(betas[1,], matrix(y[idx[1]],nrow=1), matrix(X[idx[1],],nrow=1), mi[idx[1]])
   
   for (iter in 2:maxiter){
     
-    step_size <- C*(iter+t0)^(-alpha)
+    step.size <- C*(iter+t0)^(-alpha)
     
-    gradient <- grad_loglik(betas[iter-1,], matrix(y[idx[iter]],nrow=1), matrix(X[idx[iter],],nrow=1), mi[idx[iter]])
-    betas[iter,] <- betas[iter-1,] - step_size*gradient
-    ll <- log_lik(betas[iter,], matrix(y[idx[iter]],nrow=1), matrix(X[idx[iter],],nrow=1), mi[idx[iter]])
-    av_ll[iter] <- ((av_ll[iter-1])*(iter-1) + ll)/iter
+    gradient <- grad.loglik(betas[iter-1,], matrix(y[idx[iter]],nrow=1), matrix(X[idx[iter],],nrow=1), mi[idx[iter]])
+    betas[iter,] <- betas[iter-1,] - step.size*gradient
+    ll <- log.lik(betas[iter,], matrix(y[idx[iter]],nrow=1), matrix(X[idx[iter],],nrow=1), mi[idx[iter]])
+    av.ll[iter] <- ((av.ll[iter-1])*(iter-1) + ll)/iter
     
     # Convergence check
-    if (abs(av_ll[iter-1] - av_ll[iter])/(av_ll[iter-1] + 1E-10) < tol){
+    if (abs(av.ll[iter-1] - av.ll[iter])/(av.ll[iter-1] + 1E-10) < tol){
       cat('Algorithm has converged after', iter, 'iterations')
       av_ll <- av_ll[1:iter]
       betas <- betas[1:iter,]
       break;
     }
 
-    else if (iter == maxiter & abs(av_ll[iter-1] - av_ll[iter])/(av_ll[iter-1] + 1E-10) >= tol){
+    else if (iter == maxiter & abs(av.ll[iter-1] - av.ll[iter])/(av.ll[iter-1] + 1E-10) >= tol){
       print('WARNING: algorithm has not converged')
       break;
     }
   }
   
-  return(list("ll" = av_ll, "beta" = betas))
+  return(list("ll" = av.ll, "beta" = betas))
 }
