45bb3a623a73a487b3a55fb66ecf0ad2087a1cf6
gpaulon <giorgiopaulon90@gmail.com>
2016-10-18 10:05:15 -0500

    Upload Exercise 6

--- HW6/HW6.R
+++ HW6/HW6.R
@@ -19,6 +19,17 @@ p <- dim(X)[2]
 beta0 <- rep(0, p)
 lambda = 0.5
 
-lasso <- proximal.gradient(y, X, beta0, lambda)
-  
+lasso1 <- proximal.gradient(y, X, beta0, lambda = 1, gamma = 1E-2, maxiter = 10000, tol = 1E-8)
 
+par(mar=c(4,4,2,2))
+plot(lasso1$ll, type = 'l', col = 'blue', lwd = 2, log = 'x', xlab = 'Iterations', ylab = 'Objective')
+points(length(lasso1$ll), lasso1$ll[length(lasso1$ll)], pch = 16, cex = 0.8, col = 'blue')
+
+lasso2 <- acc.proximal.gradient(y, X, beta0, lambda = 1, gamma = 1E-4, maxiter = 10000, tol = 1E-8)
+
+lines(lasso2$ll, col = 'red', type = 'l', lwd = 2)
+points(length(lasso2$ll), lasso2$ll[length(lasso2$ll)], pch = 16, cex = 0.8, col = 'red')
+
+fit <- glmnet(X, y, family = 'gaussian', alpha = 1, lambda = 1/n, standardize = FALSE, intercept = FALSE)
+
+cbind(lasso1$betas, lasso2$betas, fit$beta)
--- HW6/proximal_gradient.R
+++ HW6/proximal_gradient.R
@@ -1,15 +1,15 @@
 
 log.lik <- function(beta, y, X, lambda){
-  # Computes the negative log-likelihood of the logit model
-  # -------------------------------------------------------
+  # Computes the objective function of the logit model
+  # --------------------------------------------------
   # Args: 
   #   - beta: regression parameters (length p)
   #   - y: response vector (length n)
   #   - X: matrix of the features (n*p)
   #   - lambda: penalization parameter
   # Returns: 
-  #   - ll: the scalar associated with the negative log-likelihood of the data
-  # --------------------------------------------------------------------------
+  #   - ll: the scalar associated with the objective function at beta
+  # -----------------------------------------------------------------
   Xbeta <- X %*% beta
   ll <- (1/2)*crossprod(y - Xbeta) + lambda * sum(abs(beta))
   return(ll)
@@ -47,7 +47,7 @@ soft.thresholding <- function(x, lambda){
 }
 
 
-proximal.gradient <- function(y, X, beta0, lambda, gamma = 0.01, maxiter = 50000, tol = 1E-10){
+proximal.gradient <- function(y, X, beta0, lambda, gamma = 1E-4, maxiter = 50000, tol = 1E-10){
   # Proximal gradient algorithm that allows to compute the Lasso regression estimates
   # ---------------------------------------------------------------------------------
   # Args: 
@@ -78,7 +78,7 @@ proximal.gradient <- function(y, X, beta0, lambda, gamma = 0.01, maxiter = 50000
     
     # Compute the gradient
     gradient <- grad.loglik(betas[iter-1, ], y, X)
-    u <- betas[iter-1, ] + gamma * gradient
+    u <- betas[iter-1, ] - gamma * gradient
     
     # Update beta
     betas[iter, ] <- soft.thresholding(u, gamma * lambda)
@@ -98,11 +98,69 @@ proximal.gradient <- function(y, X, beta0, lambda, gamma = 0.01, maxiter = 50000
       break;
     }
   }
-  return(list("ll" = ll, "betas" = betas))
+  return(list("ll" = ll, "betas" = betas[iter,]))
 }
 
 
-
+acc.proximal.gradient <- function(y, X, beta0, lambda, gamma = 1E-4, maxiter = 50000, tol = 1E-10){
+  # Accelerated proximal gradient algorithm that allows to compute the Lasso regression estimates
+  # ---------------------------------------------------------------------------------------------
+  # Args: 
+  #   - y: response vector (length n)
+  #   - X: matrix of the features (n*p)
+  #   - beta0: initial values of beta
+  #   - lambda: penalization parameter for the lasso (threshold)
+  #   - gamma: proximal operator parameter
+  #   - maxiter: maximum number of iterations
+  #   - tol: tolerance defining convergence
+  # Returns: 
+  #   - ll: values of the objective function for each iteration
+  #   - betas: values of the beta parameters for each iteration
+  # -----------------------------------------------------------
+  
+  # Check if the inputs have the correct sizes
+  if ((length(y) != dim(X)[1]) || (length(beta0) != dim(X)[2])){
+    stop("Incorrect input dimensions.")
+  }
+  
+  # Initialize the data structures
+  betas <- array(NA, dim=c(maxiter, length(beta0)))
+  betas[1,] <- beta0 # Initial guess
+  z <- array(NA, dim=c(maxiter, length(beta0)))
+  z[1,] <- beta0
+  s <- array(NA, dim = maxiter)
+  s[1] <- 1
+  ll <- array(NA, dim = maxiter)
+  ll[1] <- log.lik(betas[1,], y, X, lambda)
+  
+  for (iter in 2:maxiter){
+    
+    # Compute the gradient
+    gradient <- grad.loglik(z[iter-1, ], y, X)
+    u <- z[iter-1, ] - gamma * gradient
+    
+    # Update beta
+    betas[iter, ] <- soft.thresholding(u, gamma * lambda)
+    s[iter] <- (1 + sqrt(1 + 4 * s[iter-1]^2))/2
+    z[iter, ] <- betas[iter, ] + ((s[iter-1] - 1)/s[iter]) * (betas[iter, ] - betas[iter-1, ])
+      
+    # Compute log-likelihood
+    ll[iter] <- log.lik(betas[iter,], y, X, lambda)
+    
+    # Convergence check
+    if ( abs(ll[iter-1] - ll[iter]) / (ll[iter-1] + 1E-3) < tol ){
+      cat('Algorithm has converged after', iter, 'iterations')
+      ll <- ll[1:iter]
+      betas <- betas[1:iter, ]
+      break;
+    }
+    else if ( (iter == maxiter) & (abs(ll[iter-1] - ll[iter]) / (ll[iter-1] + 1E-3) >= tol) ){
+      print('WARNING: algorithm has not converged')
+      break;
+    }
+  }
+  return(list("ll" = ll, "betas" = betas[iter,]))
+}
 
 
 
